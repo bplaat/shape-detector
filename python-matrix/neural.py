dc64e21e0b252cae963c6e437fff5c11095391d3
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2022-05-19 10:59:20 +0200

    Used the right sigmoid dirivative function which runs much faster

--- python-matrix/neural.py
+++ python-matrix/neural.py
@@ -6,7 +6,7 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 def sigmoid_derivative(x):
-    return x * (1 - x)
+    return sigmoid(x) * (1 - sigmoid(x))
 
 def relu(x):
     return np.maximum(0, x)
